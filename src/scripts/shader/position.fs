15fa6850fb851e44e9a55966df8bd950ce5aed78
nemutas <inohiro.focru@gmail.com>
2024-08-31 21:56:53 +0900

    .

--- src/scripts/shader/position.fs
+++ src/scripts/shader/position.fs
@@ -29,9 +29,7 @@ void main() {
     float radius = (h.y * (0.5 - 0.4) + 0.4);
     pos = normalize(hash(vec3(vUv, time)) * 2.0 - 1.0) * radius;
   } else {
-    // life -= 0.01;
     life -= deltaTime * 0.8;
-    // pos += curl(pos * 0.35, time, 0.1 + (1.0 - life) * 0.1) * 0.03;
     pos += curl(pos * 0.35, time * 0.5, 0.1 + (1.0 - life) * 0.1) * 0.03;
   }
 
